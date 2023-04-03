32ca90dacbbef214466fdeae50d1d86fd51144aa
SaturnX77 <saturnvdt@gmail.com>
2023-04-03 15:45:07 -0700

    base item class created. generates rarity and value for all items

--- src/Item.java
+++ src/Item.java
@@ -2,18 +2,51 @@ public class Item {
 
     String rarity;
     int value;
-    public Item(){
-
+    public Item(int characterLuck){
+        setRarity(characterLuck);
+        setValue(rarity);
     }
 
-    public void setRarity() {
-        NumberProcessor.getRandom(0,4);
-        switch ()
+    private void setRarity(int luck) {
+        String temp;
+        int randomNum = NumberProcessor.getRandom(0,101);
+        randomNum = randomNum*(luck/100);
+        if(randomNum <= 30){
+            temp = "Common";
+        } else if(randomNum <= 55){
+            temp = "Uncommon";
+        } else if(randomNum <= 75){
+            temp = "Rare";
+        } else if(randomNum <= 90){
+            temp = "Very Rare";
+        } else if(randomNum <= 98){
+            temp = "Legendary";
+        } else {
+            temp = "Artifact";
+        }
+        rarity = temp;
+    }
+    public String getRarity() {
+        return rarity;
     }
 
-    private void setValue() {
+    private void setValue(String rarity) {
+        switch (rarity){
+            case "Common" : value = (int)(100.0 / (NumberProcessor.getRandom(40,100)/100));
+                break;
+            case "Uncommon" : value = (int)(250.0 / (NumberProcessor.getRandom(40,100)/100));
+                break;
+            case "Rare" : value = (int)(500.0 / (NumberProcessor.getRandom(40,100)/100));
+                break;
+            case "Very Rare" : value = (int)(1000.0 / (NumberProcessor.getRandom(40,100)/100));
+                break;
+            case "Legendary" : value = (int)(2500.0 / (NumberProcessor.getRandom(30,100)/100));
+                break;
+            case "Artifact" : value = (int)(7500.0 / (NumberProcessor.getRandom(30,100)/100));
 
+        }
     }
-
-
+    public int getValue() {
+        return value;
+    }
 }
--- src/PCharacter.java
+++ src/PCharacter.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class PCharacter {
     double damage = 0.0;
     double health = 100.0;
-    ArrayList<>
+    //ArrayList<>
 
     public PCharacter(){
 
