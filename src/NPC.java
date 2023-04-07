72edf465bd6c756121beff735a1c740482def11d
SaturnX77 <saturnvdt@gmail.com>
2023-04-07 15:24:24 -0700

    monster types

--- src/NPC.java
+++ src/NPC.java
@@ -5,13 +5,37 @@ public class NPC {
     double dexterity = 0.0;
     WorldGenerator.WorldType worldType;
     public enum enemyType {
-//        CAVE(0.9,0.0,0),
-//        PLAIN(0.5,0.1,0),
-//        ROAD(0.2,0.2,0),
-//        ANCIENT_RUINS(0.5,0.0,0),
-//        FOREST(0.6,0.1,0),
-//        ATLANTIS(0.7,0.0,0),
-//        CASTLE(0,0,1);
+        //CAVE
+        BASILISK(1.1,1.2,0.8,0.9),
+        BEHIR(1.4,1.3,1.2,0.5),
+        BEHOLDER(0.5,0.8,0.6,1.7),
+        UNDEAD(0.6,1.3,0.4,0.8), //ALSO IN ANCIENT RUINS
+        GIANT_BAT(1.0,1.3,0.6,1.4),
+        DRIDER(0.8,1.5,0.8,1.2),
+        //PLAIN
+        TREASURE_HUNTERS(1.0,1.0,1.0,1.0), //ALSO IN ROADS
+        BANDITS(0.8,1.2,1.0,1.1), // ALSO IN ROADS
+        SLIME(0.7,0.7,1.4,0.7),
+        UNICORN(1.4,0.5,1.2,1.3),
+        GIANT_BOAR(1.4,1.2,0.7,0.5),
+        //ROAD
+        MERCENARIES(1.0,1.1,0.7,1.2),
+        //FOREST
+        BANSHEE(0.8,1.1,0.7,1.3),
+        PIXIE(0.7,1.3,0.8,1.2),
+        ETTERCAP(1.4,1.4,1.4,0.2),
+        WEREWOLF(1.2,1.2,0.8,0.8),
+        WILL_O_WISP(0.4,1.5,0.4,1.5),
+        TREANT(1.7,0.5,1.5,0.1),
+        DIRE_WOLF(0.8,1.2,0.8,1.2),
+        //ATLANTIS
+        CHUUL(0.7,0.7,1.2,1.0),
+        KRAKEN(2.0,2.0,0.2,0.5),
+        KUO_TOA(1.0,1.0,0.8,1.0),
+        MERFOLK(1.0,1.2,0.8,1.2),
+        MERROW(1.0,0.8,0.8,1.0),
+        //CASTLE
+        DRAGON(5.0,5.0,5.0,0.1);
         final double healthScalar;
         final double attackScalar;
         final double defenseScalar;
@@ -32,6 +56,7 @@ public class NPC {
     public NPC(WorldGenerator.WorldType worldType){
         this.worldType = worldType;
     }
+
 
 
 
