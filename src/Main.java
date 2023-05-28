91f693cda2540f51bb27f9df7112868c00403fa5
SaturnX77 <saturnvdt@gmail.com>
2023-05-27 17:14:53 -0700

    Enable Auto Console + Internal File Streams

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.awt.*;
+import java.io.Console;
+import java.io.IOException;
+
 public class Main {
     public static PCharacter character;
     public static void main(String[] args) {
+        Console console = System.console();
+        if(console == null && !GraphicsEnvironment.isHeadless()){
+            String filename = Main.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6);
+            try {
+                Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""});
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
         //System.out.println("\033[107m\033[30m");
         character = new PCharacter();
         UserInteraction userInteraction = new UserInteraction();
